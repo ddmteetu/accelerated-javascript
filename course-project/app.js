2a608e307cf2027027390a0a9c15007c5819bf6b
Vishvendra Singh <ddmteetu@gmail.com>
2021-04-29 17:05:14 +0530

    Fetching and Handling Weather Data

--- course-project/app.js
+++ course-project/app.js
@@ -8,16 +8,20 @@ function searchWeather() {
     return alert('Please enter a City Name');
   }
   var http = new XMLHttpRequest();
-  var apikey = '';
-  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
+  var apiKey = '';
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
   var method = 'GET';
 
   http.open(method, url);
   http.onreadystatechange = function() {
     if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
       var data = JSON.parse(http.responseText);
+      var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+      weatherData.temperature = data.main.temp;
+      console.log(weatherData);
     } else if (http.readyState === XMLHttpRequest.DONE) {
       alert('Something went wrong!');
     }
-  }
+  };
+  http.send();
 }
